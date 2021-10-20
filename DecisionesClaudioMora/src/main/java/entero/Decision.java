7a89fcede34a0a43c32dceb5b16220f482106497
ceftx <claudiocmorab@gmail.com>
2021-10-19 21:22:45 -0400

    primer ejericicio listo

--- DecisionesClaudioMora/src/main/java/entero/Decision.java
+++ DecisionesClaudioMora/src/main/java/entero/Decision.java
@@ -5,13 +5,25 @@ import java.util.Scanner;
  *
  * @author ceftx
  */
+
 public class Decision {
+    
+    public static void validaNumero(int n){
+        if(n > 0){
+            System.out.println("El número ingresado es positivo.");
+        }else{
+            System.out.println("El número ingresado es negativo.");
+        }
+    }
     
     public static void main(String Args[]){
         Scanner ent = new Scanner(System.in);
-        int x = 0;
-        x = ent.nextInt();
+        int nume = 0; 
+        System.out.println("Ingrese un número: ");
+        nume = ent.nextInt();
+        System.out.println("A continuación, se verificará si es un numero" +
+                " positivo o negativo.");
+        validaNumero(nume);
         
-        System.out.println('\n' + x);
     }
 }
