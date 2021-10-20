c927fc58ac2a28ef26be7af5c188cda5223bbc13
ceftx <claudiocmorab@gmail.com>
2021-10-19 20:33:59 -0400

    segunda carga

---
+++ DecisionesClaudioMora/src/main/java/entero/Decision.java
@@ -0,0 +1,17 @@
+
+package entero;
+import java.util.Scanner;
+/**
+ *
+ * @author ceftx
+ */
+public class Decision {
+    
+    public static void main(String Args[]){
+        Scanner ent = new Scanner(System.in);
+        int x = 0;
+        x = ent.nextInt();
+        
+        System.out.println('\n' + x);
+    }
+}
